b49693f8b5cdc2445c92da0b596b99ab965ed2f5
NihadUser <kermovnihad.com>
2025-01-18 23:34:38 +0400

    Addin migration adn user model

---
+++ WinFormsApp2/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WinFormsApp2.Models;
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Replace with your database connection string
+        optionsBuilder.UseSqlServer("Data Source=DESKTOP-10DFVI1;Initial Catalog=ecommerce;Integrated Security=True;Trust Server Certificate=True");
+    }
+}
+
